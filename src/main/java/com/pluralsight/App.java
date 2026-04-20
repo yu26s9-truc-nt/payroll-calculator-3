1e6a01f7364e8ee3e792175bf375a4239258f5ba
truc-nt <truc.ntw@gmail.com>
2026-04-20 17:00:29 +0000

    add

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("employees.csv");
+        BufferedReader br = new BufferedReader(reader);
+
+        String line;
+        while((line = br.readLine()) != null) {
+            String[] parts = line.split("\\|");
+            if (parts[0].equals("id")) continue;
+
+            int employeeId = Integer.parseInt(parts[0]);
+            String name = parts[1];
+            double hoursWorked = Double.parseDouble(parts[2]);
+            double payRate = Double.parseDouble(parts[3]);
+            Employee employee = new Employee(employeeId, name, hoursWorked, payRate);
+            System.out.printf("ID: %d, Name: %s, Gross Pay: %.2f\n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+        }
+    }
+}
---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class Employee 
+{
+    int employeeId;
+    String name;
+    double hoursWorked;
+    double payRate;
+    
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+}
